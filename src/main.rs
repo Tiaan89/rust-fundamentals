298275741ae827df45be40fce146dab8d990c078
Dirk Robinson <tiaan.robinson@gmail.com>
2022-05-06 14:07:37 +0200

    Variables, Data Types and Macros

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+fn add(num_one: i32, num_two: i32) -> i32 {
+    num_one + num_two
 }
+
+fn main() {
+    let foo = add(10, 5);
+    println!("{:?}", foo); 
+// ! indicates macro 
+//   let mut my_name = "Dirk";
+//    my_name = "Robinson";
+}
+
