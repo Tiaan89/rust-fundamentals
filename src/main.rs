a2fbcafab81424ed102620be04253e5126a27f47
Dirk Robinson <tiaan.robinson@gmail.com>
2022-05-08 11:24:47 +0200

    Control Flow and Match statements

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,33 @@ fn add(num_one: i32, num_two: i32) -> i32 {
 }
 
 fn main() {
-    let foo = add(10, 5);
-    println!("{:?}", foo); 
-// ! indicates macro 
+    let mut total = add(20, 5);
+    let mut free_shipping = false;
+
+    if total > 50 {
+        println!("You qualify for free shippong!");
+        free_shipping = true;
+    }
+    else if total > 20 {
+        println!("If you add more items, you can qualify for free shipping!");
+    }
+    else {
+        println!("No free shipping");
+    }
+
+    total = match free_shipping {
+        true => total + 0,
+        false => total + 5
+    };
+
+    match total {
+        1 => println!("1"),
+        2 => println!("2"),
+        _ => println!("No match found.")
+    }
+
+    println!("Total: {:?}", total); // ! indicates macro 
+
 //   let mut my_name = "Dirk";
 //    my_name = "Robinson";
 }
